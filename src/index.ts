8471c474f35d88ce224eee89f613916bc6a90165
evandikt <evandikt@gmail.com>
2021-09-16 15:56:49 +0200

    OOPS

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,13 @@ const main = async () => {
 
   if (!Array.isArray(conf.config)) {
     if (!conf.config.name) {
-      conf.config.name = "streamlabs";
+      conf.config.name = "slobs";
     }
     new SlobsConnexion(conf.host, conf.port, conf.config);
   } else {
     conf.config.forEach((value, index) => {
       if (!value.name) {
-        value.name = `streamlabs-${index + 1}`;
+        value.name = `slobs-${index + 1}`;
       }
       new SlobsConnexion(conf.host, conf.port, value);
     });
